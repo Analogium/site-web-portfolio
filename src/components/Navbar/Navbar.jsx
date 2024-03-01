3a9e2bdf1d482453450de0c2fa29f681e5f7fabd
Theo Lambert <theo.lambert@mmtt.fr>
2024-03-01 16:30:51 +0100

    ok

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,83 +1,86 @@
-import React, { useState } from 'react'
-import {images} from '../../constants'
-import  { HiMenuAlt4, HiX} from 'react-icons/hi'
-import {motion} from 'framer-motion'
-import {BsTwitter, BsTwitch, BsInstagram, BsFacebook, BsGithub, BsLinkedin} from 'react-icons/bs'
+import React, { useState } from 'react';
+import { images } from '../../constants';
+import { HiMenuAlt4, HiX } from 'react-icons/hi';
+import { motion } from 'framer-motion';
+import {
+  BsTwitch,
+  BsInstagram,
+  BsFacebook,
+  BsGithub,
+  BsLinkedin,
+} from 'react-icons/bs';
 
-
-import './Navbar.scss'
+import './Navbar.scss';
 
 const Navbar = () => {
-
   const [toggle, setToggle] = useState(false);
 
-
-
   return (
-    <nav className='app__navbar'>
-      <div className='app__navbar-logo'>
-        <img src={images.logo} alt="logo"/>
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.logo} alt="logo" />
       </div>
-      <ul className='app__navbar-links'>
+      <ul className="app__navbar-links">
         {['informations', 'competences', 'contact'].map((item) => (
-          <li className='app__flex p-text'  key={`link-${item}`}>
-            <a href={`#${item}`} title={`${item}`} className='link'>{item}</a>
+          <li className="app__flex p-text" key={`link-${item}`}>
+            <a href={`#${item}`} title={`${item}`} className="link">
+              {item}
+            </a>
           </li>
         ))}
       </ul>
 
-      <div className='app__navbar-menu'>
-          <HiMenuAlt4 onClick={() => setToggle(true)}/>
+      <div className="app__navbar-menu">
+        <HiMenuAlt4 onClick={() => setToggle(true)} />
 
-          {toggle && (
-            <motion.div
-              whileInView={{x : [100, 0]}}
-              transition={{ duration: 0.85, ease: 'easeOut'}}
-            >
-              <HiX onClick={() => setToggle(false)}/>
-              <ul className='menu_link'>
-                {['informations', 'competences', 'contact'].map((item) => (
-                  <li   key={item}>
-                    <a href={`#${item}`} onClick={() => setToggle(false)}>{item}</a>
-                  </li>
-                ))}
-              </ul>
-
-              <ul className='social_media_menu'>
-                <li>
-                  <a href='https://www.twitch.tv/imaginemylife' >
-                    <BsTwitch />
+        {toggle && (
+          <motion.div
+            whileInView={{ x: [100, 0] }}
+            transition={{ duration: 0.85, ease: 'easeOut' }}
+          >
+            <HiX onClick={() => setToggle(false)} />
+            <ul className="menu_link">
+              {['informations', 'competences', 'contact'].map((item) => (
+                <li key={item}>
+                  <a href={`#${item}`} onClick={() => setToggle(false)}>
+                    {item}
                   </a>
                 </li>
-                <li>
-                  <a href='https://www.instagram.com/theo__lmbt/' >
-                    <BsInstagram />
-                  </a>
-                </li>
-                <li>
-                  <a href='https://www.facebook.com/profile.php?id=100005523438840' >
-                    <BsFacebook />
-                  </a>
-                </li>
-                <li>
-                  <a href='https://github.com/Analogium' >
-                    <BsGithub />
-                  </a>
-                </li>
-                <li>
-                  <a href='https://www.linkedin.com/in/lambert-theo-6010211b9/' >
-                    <BsLinkedin />
-                  </a>
-                </li>
-              </ul>
+              ))}
+            </ul>
 
-            </motion.div>
-          )}
-
-
+            <ul className="social_media_menu">
+              <li>
+                <a href="https://www.twitch.tv/imaginemylife">
+                  <BsTwitch />
+                </a>
+              </li>
+              <li>
+                <a href="https://www.instagram.com/theo__lmbt/">
+                  <BsInstagram />
+                </a>
+              </li>
+              <li>
+                <a href="https://www.facebook.com/profile.php?id=100005523438840">
+                  <BsFacebook />
+                </a>
+              </li>
+              <li>
+                <a href="https://github.com/Analogium">
+                  <BsGithub />
+                </a>
+              </li>
+              <li>
+                <a href="https://www.linkedin.com/in/lambert-theo-6010211b9/">
+                  <BsLinkedin />
+                </a>
+              </li>
+            </ul>
+          </motion.div>
+        )}
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -1,60 +1,62 @@
-import React from 'react'
-import './Header.scss'
+import React from 'react';
+import './Header.scss';
 
-import { images } from '../../constants'
-import {BsTwitter, BsTwitch, BsInstagram, BsFacebook, BsGithub, BsLinkedin} from 'react-icons/bs'
-
-
+import { images } from '../../constants';
+import {
+  BsTwitch,
+  BsInstagram,
+  BsFacebook,
+  BsGithub,
+  BsLinkedin,
+} from 'react-icons/bs';
 
 const Header = () => {
   return (
-    <section id='Accueil'>
+    <section id="Accueil">
       <div className="app__header app__flex">
-        <div className='content'>
-          <div className='author'>
+        <div className="content">
+          <div className="author">
             <h1>Theo Lambert</h1>
           </div>
 
-          <div className='job'>
+          <div className="job">
             <h3>Développeur Web</h3>
           </div>
-
         </div>
-        <div className='logo_header'>
-          <img src={images.logo_header} alt='logo-header'/>
+        <div className="logo_header">
+          <img src={images.logo_header} alt="logo-header" />
         </div>
 
-        <ul className='social_media'>
+        <ul className="social_media">
           <li>
-            <a href='https://www.twitch.tv/imaginemylife' >
+            <a href="https://www.twitch.tv/imaginemylife">
               <BsTwitch />
             </a>
           </li>
           <li>
-            <a href='https://www.instagram.com/theo__lmbt/' >
+            <a href="https://www.instagram.com/theo__lmbt/">
               <BsInstagram />
             </a>
           </li>
           <li>
-            <a href='https://www.facebook.com/profile.php?id=100005523438840' >
+            <a href="https://www.facebook.com/profile.php?id=100005523438840">
               <BsFacebook />
             </a>
           </li>
           <li>
-            <a href='https://github.com/Analogium' >
+            <a href="https://github.com/Analogium">
               <BsGithub />
             </a>
           </li>
           <li>
-            <a href='https://www.linkedin.com/in/lambert-theo-6010211b9/' >
+            <a href="https://www.linkedin.com/in/lambert-theo-6010211b9/">
               <BsLinkedin />
             </a>
           </li>
         </ul>
-      
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
